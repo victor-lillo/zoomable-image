18cb2052ba4e837ff35bc69331dcc20643646296
fentosv <fentos.dev@gmail.com>
2023-11-30 20:50:58 +0100

    Disable Storybook addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,9 +3,9 @@ import type { StorybookConfig } from '@storybook/html-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    // '@storybook/addon-links',
+    // '@storybook/addon-essentials',
+    // '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/html-vite',
