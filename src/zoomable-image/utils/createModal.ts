7d6f25e34f3156fbe4d765699b47b58b46d35d5b
fentosv <fentos.dev@gmail.com>
2023-12-02 20:35:06 +0100

    Enhance SR accessibility

--- src/zoomable-image/utils/createModal.ts
+++ src/zoomable-image/utils/createModal.ts
@@ -7,11 +7,11 @@ export default function createModal($clickedImage: HTMLImageElement) {
   const { newHeight, newWidth } = getNewSize({ naturalHeight, naturalWidth })
 
   const $modal = document.createElement('dialog')
-  $modal.classList.add('zoom-modal')
+  $modal.classList.add('zi')
 
   const $image = document.createElement('img')
   $image.setAttribute('src', currentSrc)
-  $image.classList.add('zoom-modal-image')
+  $image.classList.add('zi-image')
   $image.alt = alt ? alt : ' '
   $image.style.width = newWidth
   $image.style.height = newHeight
@@ -22,11 +22,15 @@ export default function createModal($clickedImage: HTMLImageElement) {
   })
 
   const $closeButton = document.createElement('button')
-  $closeButton.classList.add('zoom-modal-button')
+  const $accessibilityTag = document.createElement('span')
+  $accessibilityTag.classList.add('zi-visually-hidden')
+  $accessibilityTag.textContent = 'Close zoomed image'
+  $closeButton.classList.add('zi-button')
   $closeButton.innerHTML = `<svg width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"><path stroke="none" d="M0 0h24v24H0z"/><path d="M18 6 6 18M6 6l12 12"/></svg>`
   $closeButton?.addEventListener('click', () => {
     closeAfterAnimation()
   })
+  $closeButton.append($accessibilityTag)
   $modal.append($image, $closeButton)
   document.body.append($modal)
 
