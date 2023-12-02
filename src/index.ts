b1a70649e7440329ce7b41e27b5484187539219a
fentosv <fentos.dev@gmail.com>
2023-12-02 14:06:12 +0100

    Rename exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export { default as init } from './zoomable-image'
+export { default as initZoomableImages } from './zoomable-image/index'
+export { default as zoomImage } from './zoomable-image/utils/createModal'
