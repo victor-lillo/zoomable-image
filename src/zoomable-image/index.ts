adbfbe4354f661b233e5f36aa8fe16126b64912f
fentosv <fentos.dev@gmail.com>
2023-12-03 20:23:07 +0100

    Add scrollOffset param to init function

--- src/zoomable-image/index.ts
+++ src/zoomable-image/index.ts
@@ -3,14 +3,16 @@ import createModal from './utils/createModal'
 
 const DATA = 'data-zoomable-image'
 
-function handleClick(e: Event) {
-  const $clickedImage = e.target as HTMLImageElement
-  createModal({ $clickedImage })
-}
-
-export default function init({ dataSelector = DATA }: { dataSelector?: string } = {}) {
+export default function init({
+  dataSelector = DATA,
+  scrollOffset,
+}: { dataSelector?: string; scrollOffset?: number } = {}) {
   const $images = document.querySelectorAll(`img[${dataSelector}]`)
+
   $images.forEach(($image) => {
-    $image.addEventListener('click', handleClick)
+    $image.addEventListener('click', (e) => {
+      const $clickedImage = e.target as HTMLImageElement
+      createModal({ $clickedImage, scrollOffset })
+    })
   })
 }
