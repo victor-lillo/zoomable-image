c727e1a306e502b63a04d95de9b31944694cbb75
fentosv <fentos.dev@gmail.com>
2023-12-04 13:13:37 +0100

    Add tests

--- test/zoomable-image.test.ts
+++ test/zoomable-image.test.ts
@@ -1,10 +1,78 @@
-import { describe, expect, assertType, expectTypeOf, it, test } from 'vitest'
+import { describe, expect, expectTypeOf, test, vi, beforeAll, beforeEach } from 'vitest'
+import { initZoomableImages, zoomImage } from '../src/index'
 
-import init from '../src/zoomable-image'
+beforeAll(() => {
+  // Waiting for this PR merge 👉🏽 https://github.com/jsdom/jsdom/pull/3403
+  // But... until it... 👉🏽 https://github.com/jsdom/jsdom/issues/3294#issuecomment-1196577616
+  HTMLDialogElement.prototype.show = vi.fn()
+  HTMLDialogElement.prototype.showModal = vi.fn()
+  HTMLDialogElement.prototype.close = vi.fn()
+})
 
-test('Types work properly', () => {
-  expectTypeOf(init).toBeFunction()
+beforeEach(() => {
+  document.body.innerHTML = `
+      <img
+        data-zoomable-image
+        class="image"
+        src="src/stories/assets/moon.webp"
+        alt=""
+      />`
+})
 
-  assertType(init({ dataSelector: undefined }))
-  assertType(init({ dataSelector: 'data-custom-selector' }))
+describe('Named exports works properly', () => {
+  test('initZoomableImages', () => {
+    expectTypeOf(initZoomableImages).toBeFunction()
+  })
+
+  test('zoomImage', () => {
+    expectTypeOf(zoomImage).toBeFunction()
+  })
+})
+
+describe('zoomImage works properly', () => {
+  test('creates a dialog', () => {
+    const image = document.querySelector('img')!
+    zoomImage(image)
+    const dialog = document.querySelector('dialog')
+    expect(dialog).toBeTruthy()
+  })
+
+  test('add expected class in modal', () => {
+    const image = document.querySelector('img')!
+    zoomImage(image)
+    const dialog = document.querySelector('dialog')
+    expect(dialog?.classList.contains('zi')).toBeTruthy()
+  })
+
+  test('creates an image', () => {
+    const image = document.querySelector('img')!
+    zoomImage(image)
+    const dialog = document.querySelector('dialog')
+    const modalImage = dialog?.querySelector('img')
+    expect(modalImage).toBeTruthy()
+  })
+
+  test('creates a button', () => {
+    const image = document.querySelector('img')!
+    zoomImage(image)
+    const dialog = document.querySelector('dialog')
+    const button = dialog?.querySelector('button')
+    expect(button).toBeTruthy()
+  })
+})
+
+describe('initZoomableImages works properly', () => {
+  test("doesn't create dialog on it", () => {
+    initZoomableImages()
+    const dialog = document.querySelector('dialog')
+    expect(dialog).toBeNull()
+  })
+
+  test('creates dialog on click', () => {
+    initZoomableImages()
+    const img = document.querySelector('img')
+    img?.click()
+    const dialog = document.querySelector('dialog')
+    expect(dialog).toBeTruthy()
+  })
 })
