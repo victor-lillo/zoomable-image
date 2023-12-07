86e2da2f345b49040ec4455200f3b79c7d9a2073
fentosv <fentos.dev@gmail.com>
2023-12-07 19:13:55 +0100

    Remove comment

--- src/zoomable-image/utils/getImageData.ts
+++ src/zoomable-image/utils/getImageData.ts
@@ -1,7 +1,5 @@
 export default function getImageData($image: HTMLImageElement) {
   // TODO HD image & prefetch
-  // Pegar una vuelta al mock de KeyframeEffect
-  // https://github.com/search?q=%28path%3A*.test.ts+OR+path%3A*.test.js%29+KeyframeEffect+&type=code&ref=advsearch
   const currentSrc = $image.currentSrc
   const alt = $image.alt
   const naturalHeight = $image.naturalHeight
